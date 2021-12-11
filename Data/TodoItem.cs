9cb223d49e00a0d85a19c9731247336e8514e81d
Alberto Pola <albertopola@archeacostruzioni.it>
2021-12-11 12:51:58 +0100

    20211211_ModelModify

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -3,8 +3,16 @@ public class TodoItem
 {
     [JsonProperty(PropertyName = "title")]
     public  string? Title { get; set; }
-    [JsonProperty(PropertyName = "effort")]
-    public decimal Effort {get; set;}
+
     [JsonProperty(PropertyName = "isdone")]
     public bool IsDone { get; set; }
+    [JsonProperty(PropertyName = "asset")]
+    public List<Asset> Assets {get; set;}
+    public class Asset
+    {
+        [JsonProperty(PropertyName ="assetname")]
+        public string AssetName {get; set;}
+        [JsonProperty(PropertyName = "asseteffort")]
+        public decimal AssetEffort {get; set;}
+    }
 }
